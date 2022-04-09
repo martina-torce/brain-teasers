eff5127b64b10c917a0ca8457abd36855a9d937f
Martina Torcè <85696182+martina-torce@users.noreply.github.com>
2022-04-09 13:34:37 +0100

    Update digital-root.py

--- digital-root.py
+++ digital-root.py
@@ -1,11 +1,11 @@
-# What is the sum of the digits in the sum of the first 123456789 positive integers?
+# What is the sum of the digits in the sum of the first n positive integers?
 
 # Sum of Integers formula
 def sum_of_integers(n):
     return int(n*(1+n)/2)
 
 # Digital Root recursive formula
-def digit_root(n): 
+def digital_root(n): 
     return (n - 1) % 9 + 1
 
 # Test
